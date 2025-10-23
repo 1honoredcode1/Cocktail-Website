5e775cc500dcebebbc127d4d698b12f93ea8d284
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-10-23 20:06:45 +0200

    its a reLIIEF this part is done, heh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <main>
       <NavBar />
+      <Hero />
     </main>
   );
 }
