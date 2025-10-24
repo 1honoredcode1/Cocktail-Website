aa8a27d2161fc8879bac258106c9c58a1c0033aa
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-10-24 20:43:27 +0200

    its official, im drunk

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -34,7 +34,7 @@ const Art = () => {
   return (
     <div id="art">
       <div className="container mx-auto h-full pt-20">
-        <h2 className="will-fade">The ART</h2>
+        <h2 className="will-fade">MIXOLOGY</h2>
 
         <div className="content">
           <ul className="space-y-4 will-fade">
